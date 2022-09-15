a07b29a3b053df39ee25b6852b2dd14606548834
MHRafix <fmhrafiz27@gmail.com>
2022-09-15 08:52:00 +0600

    production push:-

--- controllers/get/cartProducts.js
+++ controllers/get/cartProducts.js
@@ -3,8 +3,7 @@ const cartCollection = require('../../models/cartModel');
 exports.cartProducts = async (req, res, next) => {
 	try {
 		const query = { userEmail: req.params.email };
-		const cart_products = await cartCollection.find(query);
-		console.log(req.params.email);
+		const cart_products = await cartCollection.find();
 		res.status(200).json(cart_products);
 	} catch (err) {
 		next(err);
